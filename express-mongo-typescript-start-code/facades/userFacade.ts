e715fd0096f1a12274136787083e5613f4454295
Chris <Stofferdyr@gmail.com>
2020-03-07 19:27:51 +0100

    start exercise b

--- express-mongo-typescript-start-code/facades/userFacade.ts
+++ express-mongo-typescript-start-code/facades/userFacade.ts
@@ -1,17 +1,63 @@
+const bcrypt = require("bcryptjs");
+const debug = require("debug")("game-case");
 
+interface IGameUser
+{
+  name: string,
+  userName: string,
+  password: string,
+  role: string
+}
 
-interface IGameUser { name: string, userName: string, password: string, role: string }
- 
 const users: Array<IGameUser> = [];
-class UserFacade {
-  static addUser(user: IGameUser): boolean {
-    /*Info: Import bcrypt and (npm install bcrypt) and hash before you store */
-    throw Error("Not Implemented")
+class UserFacade
+{
+  static addUser(user: IGameUser): boolean
+  {
+    const saltAmount = 15;
+    let succes = false;
+    const setPW = async (user: IGameUser) =>
+    {
+      await bcrypt.hash(user.password, saltAmount, (err, hash) =>
+      {
+        if (err)
+        {
+          debug(`We're out of hash: ${ err }`);
+        }
+        else
+        {
+          user.password = hash;
+          users.push(user);
+          succes = true;
+        }
+      });
+    };
+    setPW(user);
+    return succes;
   }
-  static deleteUser(userName: string): boolean { throw Error("Not Implemented") }
-  static getAllUsers(): Array<IGameUser> { throw new Error("Not yet implemented") }
+  static deleteUser(userName: string): boolean
+  {
+    const index = users.findIndex(user =>
+    {
+      return user.name === userName;
+    });
+    if (index)
+    {
+      users.splice(index, 1);
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  static getAllUsers(): Array<IGameUser> 
+  {
+    return users;
+  }
   static getUser(userName: string): IGameUser { throw new Error("Not yet implemented") }
-  static checkUser(userName: string, password: string): boolean {
+  static checkUser(userName: string, password: string): boolean
+  {
     /*Use bcrypts compare method */
     throw new Error("Not yet implemented")
   }
